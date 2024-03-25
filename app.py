d370d99247959ca5dcaa042619e693010113aef0
Yathish S <61156886+yathish-debug@users.noreply.github.com>
2024-03-26 01:33:35 +0530

    app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from htmlTemplates import *  # Import HTML templates
 from openai import OpenAI
 import openai
 from langchain.llms import OpenAI
-
+os.environ["OPENAI_API_KEY"] = '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'
 # Function to extract text from PDF documents
 def get_pdf_text(pdf_docs):
     text = ""
